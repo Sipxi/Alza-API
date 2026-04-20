e267be08017b0d6db39da52fe0b16c3e0b2a58e5
Sipxi <sipxi@email.cz>
2026-04-20 19:27:32 +0200

    feat: add product entity

---
+++ AlzaApi.DAL/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlzaApi.DAL.Entities
+{
+    public class Product : BaseEntity
+    {
+        [StringLength(50)]
+        public required string Name { get; set; }
+
+        [StringLength(1000)]
+        public required string ImgUri { get; set; }
+
+        public required Decimal Price { get; set; }
+
+        [StringLength(2048)]
+        public string? Description { get; set; }
+    }
+}
