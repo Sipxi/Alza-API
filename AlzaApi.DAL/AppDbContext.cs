c3c84cddc0d9fdeadf439d50268940960cbff836
Sipxi <sipxi@email.cz>
2026-04-20 19:53:45 +0200

    feat: add AppDbContext

---
+++ AlzaApi.DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AlzaApi.DAL.Configurations;
+using AlzaApi.DAL.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AlzaApi.DAL;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ AlzaApi.DAL/Configurations/ProductConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using AlzaApi.DAL.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlzaApi.DAL.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        // Table name
+        builder.ToTable("Products");
+
+        // Primary key
+        builder.HasKey(p => p.Id);
+
+        // Name rules
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        // Price rules
+        builder.Property(p => p.Price)
+            .IsRequired()
+            .HasPrecision(10, 2);
+
+        // Image
+        builder.Property(p => p.ImgUri)
+            .IsRequired()
+            .HasMaxLength(2048);
+
+        // Description
+        builder.Property(p => p.Description)
+            .HasMaxLength(1000);
+        
+        // Creating, in bigger systems can centralize this
+        builder.Property(p => p.CreatedAt)
+            .HasDefaultValueSql("GETUTCDATE()");
+    }
+}
