a7c39e6f53b2a9a8cc19fd94717b69793921aa84
Sipxi <sipxi@email.cz>
2026-04-21 01:42:22 +0200

    feat: add controllers

---
+++ AlzaApi.App/Controllers/V1/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using AlzaApi.BL.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlzaApi.App.Controllers.V1;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[ApiExplorerSettings(GroupName = "v1")] // Подсказка для Swagger, чтобы он не путался
+public class ProductController(IProductService productService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetProducts()
+    {
+        var products = await productService.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id:guid}/")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var product = await productService.GetByIdAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(product);
+    }
+
+    [HttpPut("{id:guid}/description")]
+    public async Task<IActionResult> UpdateDescription(Guid id, [FromBody] string description)
+    {
+        var success = await productService.UpdateDescriptionAsync(id, description);
+        if (!success) return NotFound();
+        return NoContent();
+    }
+}
---
+++ AlzaApi.App/Controllers/V2/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using AlzaApi.BL.Interfaces;
+using AlzaApi.Common.DTOs.Product;
+using AlzaApi.Common.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlzaApi.App.Controllers.V2;
+
+[ApiController]
+[Route("api/v2/[controller]")]
+[ApiExplorerSettings(GroupName = "v2")]
+public class ProductsController(IProductService productService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<PaginatedResult<ProductListDto>>> GetPaginated(
+        [FromQuery] int pageNumber = 1,
+        [FromQuery] int pageSize = 10)
+    {
+        var result = await productService.GetAllPaginatedAsync(pageNumber, pageSize);
+        return Ok(result);
+    }
+}
