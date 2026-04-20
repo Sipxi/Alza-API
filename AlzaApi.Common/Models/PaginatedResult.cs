4e0d3b1b4feb28caba33beffc1b5616454887284
Sipxi <sipxi@email.cz>
2026-04-20 20:59:31 +0200

    feat(common): add paginatedresult

---
+++ AlzaApi.Common/Models/PaginatedResult.cs
@@ -0,0 +1,12 @@
+﻿namespace AlzaApi.Common.Models;
+
+public class PaginatedResult<T>(IEnumerable<T> items, int totalItems, int pageNumber, int pageSize)
+{
+    public IEnumerable<T> Items { get; set; } = items;
+    public int TotalItems { get; set; } = totalItems;
+
+    public int PageNumber { get; set; } = pageNumber;
+    public int PageSize { get; set; } = pageSize;
+
+    public int TotalPages => (int) Math.Ceiling(TotalItems / (double) PageSize);
+}
