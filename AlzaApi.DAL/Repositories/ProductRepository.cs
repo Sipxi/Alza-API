0f20a8b155cd8412ff627661ff860ece2077917e
Sipxi <sipxi@email.cz>
2026-04-20 21:01:14 +0200

    feat: add repository for product, generic repository and interfaces

---
+++ AlzaApi.DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using AlzaApi.DAL.Entities;
+using AlzaApi.DAL.Interfaces;
+
+namespace AlzaApi.DAL.Repositories;
+
+public class ProductRepository(AppDbContext dbContext) : BaseRepository<Product>(dbContext),
+    IProductRepository
+{
+    public async Task UpdateProductDescriptionAsync(Guid productId, string description)
+    {
+        var product = await _dbSet.FindAsync(productId);
+        if (product != null)
+        {
+            product.Description = description;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
