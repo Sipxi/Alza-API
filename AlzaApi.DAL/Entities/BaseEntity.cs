649bcad0ff21c9e14cbe4a282941e927ae690da0
Sipxi <sipxi@email.cz>
2026-04-20 19:54:24 +0200

    fix: moved constrains to configuration

--- AlzaApi.DAL/Entities/BaseEntity.cs
+++ AlzaApi.DAL/Entities/BaseEntity.cs
@@ -4,5 +4,4 @@ public class BaseEntity
 {
     public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
 }
--- AlzaApi.DAL/Entities/Product.cs
+++ AlzaApi.DAL/Entities/Product.cs
@@ -1,18 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace AlzaApi.DAL.Entities
+﻿namespace AlzaApi.DAL.Entities
 {
     public class Product : BaseEntity
     {
-        [StringLength(50)]
-        public required string Name { get; set; }
+        public string Name { get; set; } = null!;
 
-        [StringLength(1000)]
-        public required string ImgUri { get; set; }
+        public string ImgUri { get; set; } = null!;
 
-        public required Decimal Price { get; set; }
+        public Decimal Price { get; set; }
 
-        [StringLength(2048)]
         public string? Description { get; set; }
     }
 }
