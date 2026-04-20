657bd3b40052ab33174d6de472ca505ad7d9cf68
Sipxi <sipxi@email.cz>
2026-04-20 17:35:35 +0200

    chore: setup project templates

---
+++ AlzaApi.App.EndToEndTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace AlzaApi.App.EndToEndTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ AlzaApi.BL.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace AlzaApi.BL.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
