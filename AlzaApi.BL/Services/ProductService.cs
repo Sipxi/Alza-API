40e756278e1f7e91d0c354ed08a693be358c7a05
Sipxi <sipxi@email.cz>
2026-04-21 00:19:38 +0200

    feat: ProductService, interface, Mapster

---
+++ AlzaApi.BL/Services/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using AlzaApi.BL.Interfaces;
+using AlzaApi.Common.DTOs.Product;
+using AlzaApi.Common.Models;
+using AlzaApi.DAL.Interfaces;
+
+using Mapster;
+
+namespace AlzaApi.BL.Services;
+
+public class ProductService(IProductRepository productRepository) : IProductService
+{
+    public async Task<IEnumerable<ProductListDto>> GetAllAsync()
+    {
+        var products = await productRepository.GetAllAsync();
+        return products.Adapt<IEnumerable<ProductListDto>>();
+    }
+
+    public async Task<ProductDetailDto?> GetByIdAsync(Guid id)
+    {
+        var product = await productRepository.GetByIdAsync(id);
+        return product?.Adapt<ProductDetailDto>();
+    }
+
+    public async Task<bool> UpdateAsync(Guid id, ProductUpdateDto updateDto)
+    {
+        var product = await productRepository.GetByIdAsync(id);
+        if (product is null)
+        {
+            return false;
+        }
+
+        await productRepository.UpdateProductDescriptionAsync(id, updateDto.Description);
+        return true;
+    }
+
+    public async Task<PaginatedResult<ProductListDto>> GetAllPaginatedAsync(int pageNumber, int
+        pageSize)
+    {
+        var pagedEntities = await productRepository.GetAllPaginatedAsync(pageNumber, pageSize);
+        var pagedDtos = pagedEntities.Items.Adapt<IEnumerable<ProductListDto>>();
+        return new PaginatedResult<ProductListDto>(pagedDtos, pagedEntities.TotalItems,
+            pagedEntities.PageNumber, pagedEntities.PageSize);
+    }
+}
