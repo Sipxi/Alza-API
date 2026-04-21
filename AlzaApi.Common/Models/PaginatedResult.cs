d3b46827f4a936891621c8e78f9d788ddc076ffc
Sipxi <sipxi@email.cz>
2026-04-21 03:20:44 +0200

    fix(common): change set -> init in paginatedresult

--- AlzaApi.Common/Models/PaginatedResult.cs
+++ AlzaApi.Common/Models/PaginatedResult.cs
@@ -2,11 +2,11 @@
 
 public class PaginatedResult<T>(IEnumerable<T> items, int totalItems, int pageNumber, int pageSize)
 {
-    public IEnumerable<T> Items { get; set; } = items;
-    public int TotalItems { get; set; } = totalItems;
+    public IEnumerable<T> Items { get; init; } = items;
+    public int TotalItems { get; init; } = totalItems;
 
-    public int PageNumber { get; set; } = pageNumber;
-    public int PageSize { get; set; } = pageSize;
+    public int PageNumber { get; init; } = pageNumber;
+    public int PageSize { get; init; } = pageSize;
 
     public int TotalPages => (int) Math.Ceiling(TotalItems / (double) PageSize);
 }
